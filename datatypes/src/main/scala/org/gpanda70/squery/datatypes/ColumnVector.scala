0b48e6382b8fac52aefdbbdadf62f819442651bc
james <jamimdev@gmail.com>
2024-01-05 19:25:48 -0500

    Adding these ColumnVector trait to be an interface for any ColumnVector implementation

---
+++ datatypes/src/main/scala/org/gpanda70/squery/datatypes/ColumnVector.scala
@@ -0,0 +1,15 @@
+package org.gpanda70.squery.datatypes
+
+import org.apache.arrow.vector.types.pojo.ArrowType
+
+/**
+ * An interface to provide convenient accessor methods, avoiding the
+ * need to case to a specific [[FieldVector]] implementation for each data type
+ *
+ */
+trait ColumnVector {
+  def getType(): ArrowType
+  def getValue(i: Int): Any
+  def getSize(): Int
+
+}
