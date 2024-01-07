228ebd4d8c2193958cab7e3134aaf3e2d47fc9ed
james <jamimdev@gmail.com>
2024-01-07 18:14:38 -0500

    Adding testcases for LiteralValueVector

---
+++ datatypes/src/test/scala/LiteralValueVectorSuite.scala
@@ -0,0 +1,32 @@
+import org.gpanda70.squery.datatypes.ArrowTypes.Int8Type
+import org.gpanda70.squery.datatypes.LiteralValueVector
+import org.scalatest.funsuite.AnyFunSuite
+
+class LiteralValueVectorSuite extends AnyFunSuite{
+
+  test("getValue should return scalar value") {
+    val value = 42
+    val size = 10
+
+    val literalValueVector = new LiteralValueVector(Int8Type, value, size)
+
+    assert(literalValueVector.getValue(0) === value)
+    assert(literalValueVector.getValue(size-1) === value)
+  }
+
+  test("getValue should throw IndexOutOfBounds exception for invalid index") {
+    val value = 42
+    val size = 10
+
+    val literalValueVector = new LiteralValueVector(Int8Type, value, size)
+
+    assertThrows[IndexOutOfBoundsException] {
+      literalValueVector.getValue(-1)
+    }
+    assertThrows[IndexOutOfBoundsException] {
+      literalValueVector.getValue(size)
+    }
+
+  }
+
+}
